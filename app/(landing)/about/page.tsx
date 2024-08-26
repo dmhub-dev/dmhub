fd832cd5d4bf65b4a971a1aba9819e658096148e
titanckw <karikuclemo@gmail.com>
2024-08-26 17:57:57 +0300

    About editing

--- app/(landing)/about/page.tsx
+++ app/(landing)/about/page.tsx
@@ -14,7 +14,7 @@ const About = () => {
           border: '1px solid #ccc',
           padding: '10px',
           minHeight: '100px',
-          width: '700px',
+          width: '650px',
           float: 'right',
           boxShadow: '0 0 5px 1px rgb(209, 209, 209)',
           marginRight: '20rem',
@@ -24,19 +24,19 @@ const About = () => {
       >
         <h1 style={{ fontSize: 30, fontFamily: 'Arial', color: 'teal', fontWeight: 'bold' }}>De kracht van MyFunus</h1>
 
-        <p style={{ marginBottom: '10px' }}>
+        <p style={{ marginBottom: '10px', color: 'dimgray', fontSize: 16 }}>
           Een slecht geregelde uitvaart of een onvolledig afscheid: een nachtmerrie voor iedere nabestaande. Een uitvaart
           vindt maar één keer plaats en maakt een belangrijk deel uit van het rouwproces van de nabestaanden, het is dus
           van immens belang dat een uitvaart perfect geregeld wordt.
         </p>
 
-        <p style={{ marginBottom: '10px' }}>
+        <p style={{ marginBottom: '10px', color: 'dimgray', fontSize: 16 }}>
           De nummer één reden voor een slechte uitvaart is de mismatch tussen nabestaanden en de uitvaartverzorger. Om deze
           reden heeft Funus zich tot doel gesteld om opdrachtgevers in staat te stellen om de uitvaartverzorger te vinden
           die bij hun past zodat uitvaarten met een nare bijsmaak voor de nabestaanden tot het verleden gaan behoren.
         </p>
 
-        <p style={{ marginBottom: '10px' }}>
+        <p style={{ marginBottom: '10px', color: 'dimgray', fontSize: 16 }}>
           Door vooraf uitvaartverzorgers te vergelijken op basis van verschillende onderdelen en gemakkelijk en snel
           contact te leggen met de gekozen uitvaartverzorger wordt de kans vergroot dat nabestaanden en de
           uitvaartverzorger een klik hebben en samen voor een perfecte uitvaart kunnen zorgen. Door middel van het bieden
@@ -48,12 +48,12 @@ const About = () => {
       <div style={{ clear: 'both' }}></div>
 
       {/* Columns Section */}
-      <div style={{ marginTop: '40px', }}>
+      <div style={{ marginTop: '80px', }}>
         <div style={{ display: 'flex', justifyContent: 'space-between', marginLeft: '20rem', marginRight: '20rem' }}>
           {/* Column 1 */}
           <div style={{ width: '30%', padding: '1rem', borderLeft: '3px solid teal' }}>
             <h2 style={{ fontFamily: 'Arial', fontWeight: 'bold', color: '#333', fontSize: 20}}>Missie</h2>
-            <p>
+            <p style={{ color: 'dimgray'}}>
               De missie van Funus is om consumenten te helpen in de zoektocht naar de uitvaartverzorger die bij hun past op
               basis van persoonlijke voorkeuren. Dit komt ook terug in de slogan van Funus: “Vind de uitvaartverzorger die bij u past”.
             </p>
@@ -62,7 +62,7 @@ const About = () => {
           {/* Column 2 */}
           <div style={{ width: '30%', padding: '1rem', borderLeft: '3px solid teal'}}>
             <h2 style={{ fontFamily: 'Arial', fontWeight: 'bold', color: '#333', fontSize: 20}}>Visie</h2>
-            <p>
+            <p style={{ color: 'dimgray'}}>
               Funus wil ervoor zorgen dat consumenten (mogelijke opdrachtgevers) op basis van zo volledig mogelijke
               informatie een keuze kunnen maken voor een uitvaartverzorger. Het doel is het creëren van een optimale match
               tussen beiden zodat er een perfecte uitvaart geregeld wordt in de ogen van de klant.
@@ -72,7 +72,7 @@ const About = () => {
           {/* Column 3 */}
           <div style={{ width: '30%', padding: '1rem', borderLeft: '3px solid teal'}}>
             <h2 style={{ fontFamily: 'Arial', fontWeight: 'bold', color: '#333', fontSize: 20}}>Persoonlijke betrokkenheid</h2>
-            <p>
+            <p style={{ color: 'dimgray'}}>
               Het persoonlijke aspect speelt een steeds grotere rol bij het regelen van een uitvaart. Consumenten willen
               een uitvaartverzorger kiezen die bij hun past. Vanuit onze werkervaring in de uitvaartbranche merken wij dit
               ook. We weten hierdoor wat er speelt in deze markt en spelen daar met Funus op in.
