0a7fa35b18fdb5b1b1cc1f10e951688956279c54
titanckw <karikuclemo@gmail.com>
2024-08-30 18:08:45 +0300

    edit contact page

--- app/(landing)/contact/page.tsx
+++ app/(landing)/contact/page.tsx
@@ -7,45 +7,23 @@ const Contact = () => {
   return (
     <>
       <Hero />
-      <div
-        style={{
-          border: "1px solid #ccc",
-          padding: "10px",
-          minHeight: "100px",
-          width: "900px",
-          float: "right",
-          boxShadow: "0 0 5px 1px rgb(209, 209, 209)",
-          marginRight: "30rem",
-          marginTop: "-5rem",
-          marginBottom: "5rem",
-          backgroundColor: "white",
-        }}
-      >
-        <h1
-          style={{
-            fontSize: 40,
-            fontFamily: "Arial",
-            color: "teal",
-            fontWeight: "bold",
-          }}
-        >
-          Vraggen of suggesties?
-        </h1>
+      <div className="flex flex-col lg:flex-row justify-between items-start mt-12 mb-[5rem]">
+        <div className="border border-gray-300 p-6 min-h-[100px] lg:w-[70%] mx-60 -mt-40 shadow-[0_0_5px_1px_rgb(209,209,209)] bg-white">
+          <h1 className="text-[40px] font-bold text-teal-600 font-sans">
+            Vraggen of suggesties?
+          </h1>
 
-        <p style={{ marginBottom: "10px", color: "dimgray", fontSize: 16 }}>
-          Bij myFunus stellen we alle informatie zorgvuldig samen en verbeteren
-          we continu. We krijgen dan ook graag feedback van u als consument of
-          als uitvaartverzorger. Wij stellen uw mening enorm op prijs!
-        </p>
+          <p className="mb-2.5 text-dimgray text-[16px]">
+            Bij myFunus stellen we alle informatie zorgvuldig samen en verbeteren we continu. We krijgen dan ook graag feedback van u als consument of als uitvaartverzorger. Wij stellen uw mening enorm op prijs!
+          </p>
 
-        <ContactForm />
+          <ContactForm />
+        </div>
+
+        <div className="lg:w-[25%] mt-12 lg:mt-0 lg:ml-4">
+          <SideBar />
+        </div>
       </div>
-
-      <div style={{ width: "20%", marginLeft: "auto", marginBottom: "5rem" }}>
-        <SideBar />
-      </div>
-
-      <div style={{ clear: "both" }}></div>
     </>
   );
 };
