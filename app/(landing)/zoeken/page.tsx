648421b808b783a2c8b59d89ebd358435adf04cf
geoffOkumu <geoffocum@gmail.com>
2024-09-02 11:56:45 +0300

    fix build error

--- app/(landing)/zoeken/page.tsx
+++ app/(landing)/zoeken/page.tsx
@@ -4,7 +4,7 @@ import SearchFilters from "@/components/search/SearchFilters";
 import SearchHeader from "@/components/search/SearchHeader";
 import { getPaginatedDirectors } from "@/lib/strapi";
 import { get } from "lodash";
-import React from "react";
+import React, { Suspense } from "react";
 
 export default async function SearchPage() {
   const res = await getPaginatedDirectors(21, 1);
@@ -13,7 +13,7 @@ export default async function SearchPage() {
   const directors = get(res, "data");
 
   return (
-    <>
+    <Suspense>
       <SearchHeader resultsCount={directorsCount} />
       <section className="w-full container max-w-7xl grid md:grid-cols-[240px_1fr] gap-8 mt-4 mb-16">
         <SearchFilters />
@@ -23,6 +23,6 @@ export default async function SearchPage() {
           <FuneralDirectorsPagination pageSize={21} total={directorsCount} />
         </section>
       </section>
-    </>
+    </Suspense>
   );
 }
