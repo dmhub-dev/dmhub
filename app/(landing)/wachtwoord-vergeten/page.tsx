76ba323df115b6708bbe35ea3342a86d92496303
geoffOkumu <geoffocum@gmail.com>
2024-09-29 16:40:22 +0300

    auth & password reset designs

--- app/(landing)/wachtwoord-vergeten/page.tsx
+++ app/(landing)/wachtwoord-vergeten/page.tsx
@@ -1,9 +1,6 @@
-import LoginForm from "@/components/auth/LoginForm";
+import PasswordResetForm from "@/components/auth/PasswordResetForm";
 import Footer from "@/components/layout/Footer";
 import Hero from "@/components/layout/Hero";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import Link from "next/link";
 import React from "react";
 
 export default function LoginPage() {
@@ -12,7 +9,7 @@ export default function LoginPage() {
       <Hero />
       <section className="min-h-32 w-full bg-gray-100">
         <div className="container py-10">
-          <LoginForm />
+          <PasswordResetForm />
         </div>
       </section>
       <Footer />
