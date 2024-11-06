44deb4e48db130f4ac5b9f27b29c316db3df567d
geoffOkumu <geoffocum@gmail.com>
2024-11-06 11:18:21 +0300

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Hero from "@/components/home/Hero";
 import OurExpertise from "@/components/home/OurExpertise";
-import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/Header";
 import MainCTA from "@/components/widgets/MainCTA";
 
 export default function Home() {
